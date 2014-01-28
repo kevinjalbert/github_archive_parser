3757befb715273f2b518fd2e6385041a3266912c
Kevin Jalbert <kevin.j.jalbert@gmail.com>
2014-01-28 13:05:23 -0500

    Attempt to fix Travis-CI build issues (require ordering)
    Fix:
     * Force require of event_handler first

--- lib/github_archive_parser/initialize.rb
+++ lib/github_archive_parser/initialize.rb
@@ -7,4 +7,6 @@ require 'yajl'
 require 'hashie'
 require 'chronic'
 
+# Require event_handler first to prevent load order issues
+require_relative './event_handler'
 Dir.glob(File.dirname(__FILE__) + '/**/*.rb') { |file| require file }
